23d853cb1c8b590233643859fd3a08ba3e725d59
Nono Martínez Alonso <mundowarezweb@gmail.com>
2015-12-16 07:39:36 -0500

    Sync.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+//var elixir = require('laravel-elixir');
+
+// SCSS
+
+elixir(function(mix) {
+    //mix.sass('main.scss');
+});
+
+// nonoesp/writing
+/*
+if(true) {
+	elixir(function(mix) {
+	    mix.scripts('/assets/css/', '/css/');
+	});	
+}
+*/
